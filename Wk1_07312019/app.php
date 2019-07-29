3de7cf9c2a8678d644166f3ea8d1c3057ce29499
sloan58 <martin.sloan@karma-tek.com>
2019-07-29 08:23:31 -0400

    week 1 setup

---
+++ Wk1_07312019/app.php
@@ -0,0 +1,39 @@
+<?php
+
+try {
+    $axl = new SoapClient('../wsdl/10.5/AXLAPI.wsdl',
+        [
+            'trace' => 1,
+            'exceptions' => true,
+            'location' => '',
+            'login' => '',
+            'password' => '',
+            'stream_context' => stream_context_create([
+                    'ssl' => [
+                        'verify_peer' => false,
+                        'verify_peer_name' => false,
+//                        'ciphers' => 'SHA1'
+                    ]
+                ]
+            ),
+        ]
+    );
+} catch (SoapFault $e) {
+    var_dump($e->getMessage());
+}
+
+try {
+
+    $response = $axl->getPhone([
+
+        'name' => 'SEP123123123123',
+
+    ]);
+
+    var_dump($response);
+
+} catch (SoapFault $e) {
+
+    var_dump($e->getMessage());
+
+}
